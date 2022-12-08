ceaadb59a8838022b18f7573129bab44e38cc71e
ANTRAXMUSA <ANTRAXMUSA@ANTRAXMUSA>
2022-12-07 19:54:20 -0800

    Add project files.

---
+++ portafolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(portafolio.Startup))]
+namespace portafolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
